64efe91ef3f8547cdf1e363b78d88199c2c11f37
btwonion <btwonion@gmx.de>
2023-11-23 19:29:37 +0100

    bump version to 1.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
 }
 
 group = "fyi.pauli"
-version = "1.0.1"
+version = "1.0.2"
 description = "Kotlin.serialization library for the Minecraft Nbt format."
 val authors = listOf("btwonion", "kxmpxtxnt")
 val isSnapshot = false
